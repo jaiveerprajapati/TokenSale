637f037767106e702bbd51ddd29f3cae4b0ce069
Jaiveer Prajapati <63976040+jaiveerprajapati@users.noreply.github.com>
2023-02-10 13:01:07 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,14 +1,14 @@
-/SPDX-License-Identifier:Unlicensed
+//SPDX-License-Identifier:Unlicensed
 
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-contract SwiftCoin is ERC20 {
+contract JVRCoin is ERC20 {
     uint256 private constant INITIAL_SUPPLY = 500000000 ether;
-    string private constant NAME = "Swift";
-    string private constant SYMBOL = "SWFT";
+    string private constant NAME = "jvr";
+    string private constant SYMBOL = "JVR";
 
     constructor(address _admin) ERC20(NAME, SYMBOL) {
         _mint(_admin, INITIAL_SUPPLY);
